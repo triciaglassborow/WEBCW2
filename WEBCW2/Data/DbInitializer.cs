8baf045f1e313fa89b41f0f0f2870a139f9db964
es9dpw <es9dpw999@gmail.com>
2024-01-11 16:09:20 +0000

    commit

--- WEBCW2/Data/DbInitializer.cs
+++ WEBCW2/Data/DbInitializer.cs
@@ -13,17 +13,17 @@ namespace WEBCW2.Data
                 return;   // DB has been seeded
             }
 
+
+
+            //Can be deleted if we don't want books hard coded into the database
             var books = new Book[]
             {
                 new Book{BookTitle="Carson",Author="Alexander", Genre="Alexander",Blurb="Alexander", StartDate=DateTime.Parse("2019-09-01"),EndDate=DateTime.Parse("2019-09-01"), Image=("https://www.w3schools.com/images/w3schools_green.jpg") },
                 new Book{BookTitle="2",Author="2", Genre="2",Blurb="2", StartDate=DateTime.Parse("2019-09-01"),EndDate=DateTime.Parse("2019-09-01"), Image=("https://www.w3schools.com/images/w3schools_green.jpg") },
-                new Book{BookTitle="2",Author="2", Genre="2",Blurb="2", StartDate=DateTime.Parse("2019-09-01"),EndDate=DateTime.Parse("2019-09-01"), Image=("https://www.w3schools.com/images/w3schools_green.jpg") },
-                new Book{BookTitle="2",Author="2", Genre="2",Blurb="2", StartDate=DateTime.Parse("2019-09-01"),EndDate=DateTime.Parse("2019-09-01"), Image=("https://www.w3schools.com/images/w3schools_green.jpg") },
-                new Book{BookTitle="2",Author="2", Genre="2",Blurb="2", StartDate=DateTime.Parse("2019-09-01"),EndDate=DateTime.Parse("2019-09-01"), Image=("https://www.w3schools.com/images/w3schools_green.jpg") },
             };
-
             context.Books.AddRange(books);
             context.SaveChanges();
+
 
 
             var users = new User[]
