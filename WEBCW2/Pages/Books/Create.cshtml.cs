32c815e8e374e86858567c6688ec41e0b4aa752f
apinayanxyz <akanenthirarasa@gmail.com>
2024-01-19 23:14:26 +0000

    test

--- WEBCW2/Pages/Books/Create.cshtml.cs
+++ WEBCW2/Pages/Books/Create.cshtml.cs
@@ -26,20 +26,47 @@ namespace WEBCW2.Pages.Books
 
         [BindProperty]
         public Book Book { get; set; } = default!;
-        
+        public User Users { get; set; } = default!;
+
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-         /* if (!ModelState.IsValid || _context.Books == null || Book == null)
-            {
+            Boolean duplicateChecker = false;
+            Boolean userChecker = false;
+            //Book existingBook = await Books.FirstOrDefault(
+            //Book existingBook = await _context.Books.(
+            //b => b.BookTitle == Book.BookTitle && b.AuthorName == Book.AuthorName);
+            foreach (var item in _context.Books) 
+            { 
+                if (item.AuthorName== Book.AuthorName) 
+                {
+                    duplicateChecker = true;
+                }
                 
+            }
+            foreach (var item in _context.Users)
+            {
+                if (item.ID == Book.UserID)
+                {
+                    Book.User = item;
+                    userChecker = true;
+                }
+            }
+            /*if (!ModelState.IsValid || _context.Books == null || Book == null)
+            {
+                return Page();
             } */
+            if (!duplicateChecker && userChecker)
+            {
+                _context.Books.Add(Book);
+                await _context.SaveChangesAsync();
 
-            _context.Books.Add(Book);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("../Index");
+                return RedirectToPage("../Index");
+            }
+            else {
+                return Page();
+            }
         }
     }
 }
