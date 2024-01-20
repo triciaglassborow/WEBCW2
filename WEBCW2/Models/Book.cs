446ee0ded2882f7844e33ca455f88d941f6e833c
apinayanxyz <akanenthirarasa@gmail.com>
2024-01-20 19:34:20 +0000

    working

--- WEBCW2/Models/Book.cs
+++ WEBCW2/Models/Book.cs
@@ -8,7 +8,7 @@ namespace WEBCW2.Models
     public class Book
     {
         public int ID { get; set; }
-        public string BookTitle { get; set; }
+        public string? BookTitle { get; set; }
         public string Genre { get; set; }
         public string Blurb { get; set; }
         public string Image { get; set; }
--- WEBCW2/Models/Stat.cs
+++ WEBCW2/Models/Stat.cs
@@ -3,9 +3,9 @@
     public class Stat
     {
         public int StatID { get; set; }
-        public int UserID { get; set; }
+        public int? UserID { get; set; }
         public User? User { get; set; }
-        public int BookID { get; set; }
+        public int? BookID { get; set; }
         public Book? Book { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
