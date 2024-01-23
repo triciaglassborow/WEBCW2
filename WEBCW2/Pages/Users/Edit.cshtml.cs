8647667c04aa5d88743ea064ec3f9b57886e1eb8
apinayanxyz <akanenthirarasa@gmail.com>
2024-01-23 22:05:03 +0000

    Added css2

--- WEBCW2/Pages/Users/Edit.cshtml.cs
+++ WEBCW2/Pages/Users/Edit.cshtml.cs
@@ -43,10 +43,7 @@ namespace WEBCW2.Pages.Users
         // For more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+            
 
             _context.Attach(User).State = EntityState.Modified;
 
